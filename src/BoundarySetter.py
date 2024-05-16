2af23769c704a4fb62d9f8cba587bb6687305958
Saif <saifeldeenadel77@gmail.com>
2024-05-16 18:28:51 +0300

    🎨 Changed the way frames were handled, video logic outside classes

--- src/BoundarySetter.py
+++ src/BoundarySetter.py
@@ -1,56 +1,49 @@
 import cv2
+import numpy as np
 import copy
 
 class BoundarySetter:
-  def __init__(self, path):
+  def __init__(self):
     self.boundary = None
     self.p1, self.p2 = None, None
-
     self.carThreshold = None
     self.isCarThresholdSet = False
-    
-    self.modified = None
+    self.og = None
 
-    # Reading first frame and setting the mouse callback to track mouse movement
-    self.cap = cv2.VideoCapture(path)
-    _, self.frame = self.cap.read()
-    self.og = cv2.resize(self.frame, (self.frame.shape[1]//2, self.frame.shape[0]//2))
-    cv2.namedWindow("CarCount")
+    # Setting the mouse callback to track mouse movement
     cv2.setMouseCallback("CarCount", self.mouseCallback)
 
-    # Method that does all the boundary logic
-    self.setBoundary(self.frame)
+  def isAllSet(self) -> bool:
+    return self.getBoundary() and self.isCarThresholdSet
+  
+  def getBoundary(self) -> tuple:
+    return self.boundary
 
-  def setBoundary(self, frame):
-    self.modified = cv2.resize(self.frame, (self.frame.shape[1]//2, self.frame.shape[0]//2))
+  def getThreshold(self) -> tuple:
+    return self.carThreshold
 
-    while not self.boundary or not self.isCarThresholdSet:
-      if self.p1 and self.p2:
-        # p1 and p2 updated in mouse callback
-        cv2.rectangle(self.modified, self.p1, self.p2, (0,255,0), 1,cv2.LINE_AA)
-        
-        if self.carThreshold:
-          cv2.line(self.modified, self.carThreshold[0], self.carThreshold[1], (0,0,255), 1,cv2.LINE_AA)
+  def update(self,frame) -> np.array:
+    self.modified = frame
+    if self.p1 and self.p2:
+      cv2.rectangle(self.modified, self.p1, self.p2, (0,255,0), 1,cv2.LINE_AA)
+      
+      if self.carThreshold:
+        cv2.line(self.modified, self.carThreshold[0], self.carThreshold[1], (0,0,255), 1,cv2.LINE_AA)
 
-      cv2.imshow("CarCount", self.modified)
-      cv2.moveWindow("CarCount", 0,0)
-      if cv2.waitKey(5) == 27:
-        break
-      # print(not self.boundary and not self.isCarThresholdSet)
+    return self.modified
 
-    cv2.destroyAllWindows()
   
   def mouseCallback(self, event, x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
       if not self.p1:
         self.p1 = (x,y)
+      if self.boundary and not self.isCarThresholdSet:
+        self.isCarThresholdSet = True
 
     if event == cv2.EVENT_LBUTTONUP:
       self.boundary = (self.p1, self.p2)
-      self.carThreshold = (())
 
     if event == cv2.EVENT_MOUSEMOVE:
-      self.modified = self.og.copy()
       if self.p1 and not self.boundary:
         self.p2 = (x,y)
       elif self.boundary and not self.isCarThresholdSet:
--- src/main.py
+++ src/main.py
@@ -3,11 +3,47 @@ import cv2
 from BoundarySetter import BoundarySetter
 from CarCounter import CarCounter
 
+path = "../video/cars2.MOV"
+
 def main():
-  path = "../video/cars.MOV"
+
+  cv2.namedWindow("CarCount")
+  cv2.moveWindow("CarCount", 400,200)
+
+  cap = cv2.VideoCapture(path)
 
   # Run boundary setting
-  boundary, carThreshold = BoundarySetter(path) 
+  img = None
+  boundarySetter = BoundarySetter()
+  
+  while not boundarySetter.isAllSet():
+      _, frame = cap.read()
+      frame = cv2.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
+      img = boundarySetter.update(frame)
+
+      cv2.imshow("CarCount", img)
+      if cv2.waitKey(10) == 27:
+        break
+
+
+  while True:
+      _, frame = cap.read()
+      frame = cv2.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
+      boundary = boundarySetter.getBoundary()
+      carThreshold = boundarySetter.getThreshold()
+
+      # frame = frame[p1[1]:p2[1],p1[0]:p2[0]]
+      # img = CarCounter.update(frame, boundary, threshold)
+
+      cv2.imshow("CarCount", img)
+      if cv2.waitKey(10) == 27:
+        break
+  
+  cv2.destroyAllWindows()
+  
+  
+  carThreshold = boundarySetter.getThreshold()
+  
   
   ## Run detection
 
